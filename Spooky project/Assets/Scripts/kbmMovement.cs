8d2a18c4eb304c5b459d3851d17cb7ce098f20ba
Railrosty <tmakela.work@gmail.com>
2025-09-08 18:56:42 +0300

    Lisätty pelaajan liike

---
+++ Spooky project/Assets/Scripts/kbmMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class kbmMovemet : MonoBehaviour
+{
+    public CharacterController controller;
+ 
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 3f;
+ 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+ 
+    Vector3 velocity;
+ 
+    bool isGrounded;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+ 
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+ 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+ 
+        //right is the red Axis, foward is the blue axis
+        Vector3 move = transform.right * x + transform.forward * z;
+ 
+        controller.Move(move * speed * Time.deltaTime);
+ 
+        //check if the player is on the ground so he can jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //the equation for jumping
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+ 
+        velocity.y += gravity * Time.deltaTime;
+ 
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
