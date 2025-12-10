64ed542959d266464c9e3fb5b9303924b03fd0b4
Lauri Vesamäki <lauri.vesamaki@gmail.com>
2025-12-10 15:48:04 +0200

    Menu ja settings korjattu

--- Spooky project/Assets/Scripts/SettingsMenu.cs
+++ Spooky project/Assets/Scripts/SettingsMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using System.Linq;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -11,7 +12,16 @@ public class SettingsMenu : MonoBehaviour
     public Slider volumeSlider;
     public Toggle fullscreenToggle;
     public Dropdown resolutionDropdown;
-    Resolution[] resolutions;
+
+    [Tooltip("Aseta tähän listaan ne resoluutiot, jotka haluat pelaajalle tarjota.")]
+    public List<Vector2> allowedResolutions = new List<Vector2>()
+    {
+        new Vector2(2560, 1440),
+        new Vector2(1920, 1080),
+        new Vector2(1280, 720),  
+        new Vector2(800, 600)
+    };
+
 
     void Start(){
 
@@ -28,33 +38,40 @@ public class SettingsMenu : MonoBehaviour
         volumeSlider.value = PlayerPrefs.GetFloat("volume");
 
         
-        resolutions = Screen.resolutions;
-
         resolutionDropdown.ClearOptions();
-
         List<string> options = new List<string>();
-
         int currentResolutionIndex = 0;
 
-        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", 0);
-
-        for (int i = 0; i < resolutions.Length; i++)
+        for (int i = 0; i < allowedResolutions.Count; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            int width = (int)allowedResolutions[i].x;
+            int height = (int)allowedResolutions[i].y;
+            
+            string option = width + " x " + height;
             options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            if (width == Screen.width && height == Screen.height)
             {
                 currentResolutionIndex = i;
             }
         }
 
         resolutionDropdown.AddOptions(options);
+        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
+        
+        if (savedResolutionIndex >= allowedResolutions.Count || savedResolutionIndex < 0)
+        {
+            savedResolutionIndex = currentResolutionIndex;
+            PlayerPrefs.SetInt("ResolutionIndex", savedResolutionIndex); 
+            PlayerPrefs.Save();
+        }
+        
         resolutionDropdown.value = savedResolutionIndex;
-        SetResolution(savedResolutionIndex);
         resolutionDropdown.RefreshShownValue();
 
+        SetResolution(savedResolutionIndex);
     }
+
 
     public void Update()
     {
@@ -63,11 +80,17 @@ public class SettingsMenu : MonoBehaviour
     }
 
     public void SetResolution(int resolutionIndex){
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
 
-        PlayerPrefs.SetInt("ResolutionIndex", resolutionIndex);
-        PlayerPrefs.Save();
+        if (resolutionIndex >= 0 && resolutionIndex < allowedResolutions.Count)
+        {
+            int width = (int)allowedResolutions[resolutionIndex].x;
+            int height = (int)allowedResolutions[resolutionIndex].y;
+
+            Screen.SetResolution(width, height, Screen.fullScreen);
+
+            PlayerPrefs.SetInt("ResolutionIndex", resolutionIndex);
+            PlayerPrefs.Save();
+        }
     }
 
     public void SetVolume(float volume) {
